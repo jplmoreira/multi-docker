eb634a168382c1cb21691784a92b95101d6fa01c
jplmoreira <jplmoreira92@gmail.com>
2020-06-02 01:58:44 +0100

    Update header

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   <Router>
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Fib Calculator</h1>
+        <h1 className="App-title">Fib Calculator on Kubernetes</h1>
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page</Link>
       </header>
